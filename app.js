3333784e3cbd96aa0b016c78b802574e55249aa8
Melissa Cruz <melissa.e.cruz@gmail.com>
2025-05-03 13:34:10 -0400

    revised the author id route and added a username route

--- app.js
+++ app.js
@@ -20,6 +20,70 @@ app.use(morgan("combined"));
 //combined - show a log that is moe comprehensive
 //dev - will show simpler information
 
+//-----------------------------------------------------------------------------------
+// Site Data
+const username = "CodeSquader";
+const date = new Date();
+const year = date.getFullYear();
+const isSignedIn = true;
+
+// Array containing 3 objects, each object representing information about a specific book. This is a representation of information that would actually be stored in a database. Since we're not to databases yet, we'll use this array instead.
+const books = [
+  {
+    _id: "001",
+    title: "Midnight for Charlie Bone",
+    author: "Jenny Nimmo",
+    price: 23,
+    starRating: 4,
+    synopsis:
+      "In the first novel, 10-year-old Charlie Bone discovers that he has a special power. After accidentally encountering a photograph of a missing baby, Charlie begins to hear the voices of people in photographs. He discovers that he is a descendant of the Red King, who was an ancient magician.",
+  },
+  {
+    _id: "002",
+    title: "Akira",
+    author: "Katsuhiro Otomo",
+    price: 16,
+    starRating: 3,
+    synopsis:
+      "Akira, a dystopian saga set in Neo-Tokyo, a city recovering from thermonuclear attack where the streets have been ceded to motorcycle gangs and the rich and powerful run dangerous experiments on destructive, supernatural powers that they cannot control.",
+  },
+  {
+    _id: "003",
+    title: "Matilda",
+    author: "Roald Dahl",
+    price: 15,
+    starRating: 5,
+    synopsis:
+      "A girl gifted with a keen intellect and psychic powers uses both to get even with her callous family and free her kindly schoolteacher from the tyrannical grip of a headmistress.",
+  },
+];
+
+// Array containing 3 objects, each object representing information about an author. This is a representation of information that would actually be stored in a database. Since we're not to databases yet, we'll use this array instead.
+const authors = [
+  {
+    _id: "001",
+    firstName: "Jenny",
+    lastName: "Nimmo",
+    birthYear: 1944,
+    bio: "Jenny Nimmo is a British author of children's books, including fantasy and adventure novels, chapter books, and picture books.",
+  },
+  {
+    _id: "002",
+    firstName: "Katsuhiro",
+    lastName: "Otomo",
+    birthYear: 1954,
+    bio: "Katsuhiro Otomo is a Japanese manga artist, screenwriter, animator and film director. He is best known as the creator of Akira, in terms of both the original 1982 manga series and the 1988 animated film adaptation.",
+  },
+  {
+    _id: "003",
+    firstName: "Roald",
+    lastName: "Dahl",
+    birthYear: 1916,
+    bio: "Roald Dahl was a British author who penned 19 children's books over his decades-long writing career.",
+  },
+];
+//------------------------------------------------------------------------------------------------------------------------
+
 //---------------------Per 2: CW: Dynamic Node Review -------------------------------
 // Tell the app to use express to bundle all of the files within the  public directory
 app.use(express.static(path.join(__dirname + "/public")));
@@ -41,6 +105,12 @@ app.get("/", (request, response, next) => {
   // response.json("hello world");//will send a json message
   response.status(200).json({
     success: { message: "This route points to the Home page" },
+    //in a key of data and a value of an object that has userName, date and year
+    data:{
+        username:username,
+        date:date,
+        year:year
+    },
     statusCode: 200,
   });
 }); //the only route that you keep so that we can see what's in the browswer - I want to see something when the server starts
@@ -49,51 +119,33 @@ app.get("/admin", (request, response, next) => {
   // response.send("This route points to the Admin Console page");
   response.status(200).json({
     success: { message: "This route points to the Admin Console page" },
-    statusCode: 200,
-  });
-});
-
-app.get("/authors", (request, response, next) => {
-  // response.send("This route points to the Author page");
-  response.status(200).json({
-    success: { message: "This route points to the Author page" },
-    statusCode: 200,
-  });
-});
-
-app.get("/books", (request, response, next) => {
-  // response.send("This route points to the Books page")
-  response.status(200).json({
-    success: { message: "This route points to the Books page" },
+    // key of data and a value of an object that has isSigned as the parameter. 
+    data: {
+        isSignedIn:isSignedIn
+    },
     statusCode: 200,
   });
 });
 
 app.get("/site-routes", (request, response, next) => {
-  // response.send("This route points to the site router page")
-  response.status(200).json({
-    success: { message: "This route points to the site router page" },
-    statusCode: 200,
+    // response.send("This route points to the site router page")
+    response.status(200).json({
+      success: { message: "This route points to the site router page" },
+      statusCode: 200,
+    });
   });
-});
+  
 
-// ---------------------------Per 3: CW: Dynamic Pass Data--------------------------------/
-// Making different routes with parameters
-// Create 4 NEW GET routes that send a request, receive a response, and move to the next block of code w/ .status().json and a success message :
-// PATH: /books/create, HANDLER:"This route points to the Create Book page”
-// PATH: /books/:_id, HANDLER: "This route points to the specific book via the ID”
-// PATH: /authors/create, HANDLER: "This route points to the Create Author page”
-// PATH: /authors/:_id, HANDLER: "This route points to the specific author via the ID”//
+// ----------AWESOME AUTHORS --------------
+app.get("/authors", (request, response, next) => {
+  // response.send("This route points to the Author page");
 
-app.get("/books/create", (request, response, next) => {
   response.status(200).json({
-    success: { message: "This route points to the Create Book page" },
-    statusCode: 200,
-  });
-});
-app.get("/books/:_id", (request, response, next) => {
-  response.status(200).json({
-    success: { message: "This route points to the specific book via the ID" },
+    success: { message: "This route points to the Author page" },
+    // key of data and a value of an object that has the array as the paramete
+    data: {
+        authors:authors
+    },
     statusCode: 200,
   });
 });
@@ -103,12 +155,97 @@ app.get("/authors/create", (request, response, next) => {
     statusCode: 200,
   });
 });
+
 app.get("/authors/:_id", (request, response, next) => {
+    const params = request.params;
+    console.log(params); //this doesn't actually log unless it's in the response
+    const {_id} = params;
+    const foundAuthor = authors.find((author)=> author._id ===_id);
+    
+    if (foundAuthor) {
+        response.status(200).json({
+            success: { message: "This route points to the specific author via the ID" },
+            statusCode: 200,
+        });        
+    }else{ //Otherwise, send a 404 error with the message of "There is no book with this id", with the corresponding statusCode.
+        response.status(404).json({
+            error:{message: "There is no author with this id"}, 
+            statusCode:404
+        });
+    };
+  
+
+});
+
+//-------------------------------BONUS-------------------------Using the information in the slides regarding route parameters, can you define a route that points to the username of CodeSquader?
+
+app.get(`/:${username}`, (request, response,next) => {
+    response.status(200).json({
+        success:{message:"This route points to the CodeSquader page"},
+        statusCode: 200
+
+    });
+})
+
+
+
+// ----------BOOK BESTIES --------------
+
+app.get("/books", (request, response, next) => {
+  // response.send("This route points to the Books page")
   response.status(200).json({
-    success: { message: "This route points to the specific author via the ID" },
+    success: { message: "This route points to the Books page" },
+    // include the proper array of data that is needed to be passed in a key of data and 
+
+    data: {
+        //a value of an object that has the array as the parameter.
+        books: books
+    },
     statusCode: 200,
   });
 });
+app.get("/books/create", (request, response, next) => {
+  response.status(200).json({
+    success: { message: "This route points to the Create Book page" },
+    statusCode: 200,
+  });
+});
+
+//the colon in the /books/:_id tells the server that this is a parameter that we are passing through in the
+app.get("/books/:_id", (request, response, next) => {
+    const params = request.params; //store the request.params object in a variable
+    console.log(params);
+    const {_id} = params; //retrieve the _id from the parameters using object destructuring 
+
+    // Create a new variable called foundBook and use the .find method on books array to find the book with the given _id.
+    const foundBook = books.find((book)=>book._id ===_id);
+
+    if (foundBook) {// if...else statement to detect if there is a book found.  If the book is found, log the key of data and a value of an object that has the foundBook as the parameter after the success message. 
+        response.status(200).json({
+            success: {message:"This route points to the specific book via the ID"},
+            key:{
+                book:foundBook
+            },
+            statusCode:200
+        });
+    } else{ //Otherwise, send a 404 error with the message of "There is no book with this id", with the corresponding statusCode.
+        response.status(404).json({
+            error:{message: "There is no book with this id"}, 
+            statusCode:404
+        })
+    };
+    
+
+});
+
+
+// ---------------------------Per 3: CW: Dynamic Pass Data--------------------------------/
+// Making different routes with parameters
+// Create 4 NEW GET routes that send a request, receive a response, and move to the next block of code w/ .status().json and a success message :
+// PATH: /books/create, HANDLER:"This route points to the Create Book page”
+// PATH: /books/:_id, HANDLER: "This route points to the specific book via the ID”
+// PATH: /authors/create, HANDLER: "This route points to the Create Author page”
+// PATH: /authors/:_id, HANDLER: "This route points to the specific author via the ID”//
 
 // est
 
